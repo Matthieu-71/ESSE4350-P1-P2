a343c97eeee68482bfe3f0bef6c8819ef27fdfcc
Naufal Rafi Antares <30761095+naufalrafi@users.noreply.github.com>
2019-03-03 14:06:22 -0500

    Update Fileio.py

--- Fileio.py
+++ Fileio.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime as dt
 import winsound
+from collections import namedtuple
 
 def banner():
   print("Team members: Matthieu Durand, Naufal Rafi Antares, Yuri LASTNAME\nProgram name: PROGRAM NAME\nRevision date: 2/28/2019\nVersion: 1.00\n\nYou can check out anytime you want, but you can never leave!")
@@ -30,8 +31,8 @@ def ReadStationFile(filename)
   az_speed_max = float(f.readline().split(" = ")[1])  #read next line, register float read after " = " MAX AZIMUTH SPEED (M/S)
   el_speed_min = float(f.readline().split(" = ")[1])  #read next line, register float read after " = " MAX ELEVATION SPEED (M/S)
 
-  
-  Station = (name, stnlat, stnlong, stnalt, utc_off, az_el_nlim, azmin, azmax, elmin, elmax, az_speed_max, el_speed_max)
+  stntup = namedtuple('stntup', ['name', 'stnlat', 'stnlong', 'stnalt', 'utc_offset', 'az_el_nlim', 'azmin', 'azmax', 'elmin', 'elmax', 'az_speed_max', 'el_speed_max'])  # SETTING UP STN NAMED TUPLE
+  Station = stntup(name, stnlat, stnlong, stnalt, utc_off, az_el_nlim, azmin, azmax, elmin, elmax, az_speed_max, el_speed_max)
 return Station
   
 
@@ -41,20 +42,21 @@ def ReadNoradTLE(filename)
   line1 = str(ff.readline())
   line2 = str(ff.readline())
 
-  name = str(line0) #NAME OF SAT
-  refepoch = float(line1[18:31]) #REFERENCE EPOCH, LAST 2 DIGITS OF YEAR FOLLOWED BY NUMBER OF DAYS PASSED IN THE YEAR
-  incl = float(line2[8:15]) # INCLINATION (DEGREES)
-  raan = float(line2[17:24]) # RAAN (DEGREES)
-  eccn = float(line2[26:32]) # ECCENTRICITY (DEGREES W/ DEC. POINTS)
-  argper = float(line2[34:41]) # ARGUMENT OF PERIGEE (DEGREES)
-  meanan = float(line2[43:50]) # MEAN ANOMALY (DEGREES)
-  meanmo = float(line2[52:62]) # MEAN MOTION (REVS PER DAY)
-  ndot = float(line1[33:42]) # FIRST TIME DERIVATIVE OF THE MEAN MOTION
-  nddot6 = float(line1[44:51]) # SECOND TIME DERIVATIVE OF THE MEAN MOTION
-  bstar = float(line1[53:60]) # BSTAR DRAG TERM 
-  orbitnum = int(line1[2:6]) # SATELLITE NUMBER = ORBIT NUMBER ??
+  name = str(line0.split("    ")[0]) #NAME OF SAT
+  refepoch = float(line1[18:32]) #REFERENCE EPOCH, LAST 2 DIGITS OF YEAR FOLLOWED BY NUMBER OF DAYS PASSED IN THE YEAR
+  incl = float(line2[8:16]) # INCLINATION (DEGREES)
+  raan = float(line2[17:25]) # RAAN (DEGREES)
+  eccn = float(line2[26:33]) # ECCENTRICITY (DEGREES W/ DEC. POINTS)
+  argper = float(line2[34:42]) # ARGUMENT OF PERIGEE (DEGREES)
+  meanan = float(line2[43:51]) # MEAN ANOMALY (DEGREES)
+  meanmo = float(line2[52:63]) # MEAN MOTION (REVS PER DAY)
+  ndot = float(line1[33:43]) # FIRST TIME DERIVATIVE OF THE MEAN MOTION
+  nddot6 = line1[44:52] # SECOND TIME DERIVATIVE OF THE MEAN MOTION, ASSUMED DECIMAL PLACE REMOVED
+  bstar = line1[53:61] # BSTAR DRAG TERM 
+  orbitnum = int(line1[2:7]) # SATELLITE NUMBER = ORBIT NUMBER ??
   
-  Station = (name, refepoch, incl, raan, eccn, argper, meanan, meanmo, ndot, nddot6, bstar, orbitnum)
+  nrdtup = namedtuple('nrdtup', ['name', 'refepoch', 'incl', 'raan', 'eccn', 'argper', 'meanan', 'meanmo', 'ndot', 'nddot6', 'bstar', 'orbitnum']) # SETTING UP NORAD TLE NAMED TUPLE
+  Station = nrdtup(name, refepoch, incl, raan, eccn, argper, meanan, meanmo, ndot, nddot6, bstar, orbitnum)
 return Station  
 
  
